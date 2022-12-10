eb73a47c9fc4bfcd6311fb2539e591b00e6b0e6f
Rob van der Leek <robvanderleek@gmail.com>
2022-12-10 09:55:06 +0100

    Back to basic

--- serverless/src/functions/capture.js
+++ serverless/src/functions/capture.js
@@ -1,26 +1,28 @@
-import {doCaptureWork, allowedRequest} from '../helpers.js';
-
 export const handler = async function (event, _) {
-    if (!allowedRequest(event.queryStringParameters)) {
-        return {
-            statusCode: 403,
-            body: JSON.stringify({message: 'Go away please'})
-        };
-    }
-    const result = await doCaptureWork(event.queryStringParameters);
-    if (result.statusCode === 200) {
-        return {
-            statusCode: 200,
-            headers: {
-                "Content-Type": `image/${result.responseType}`,
-            },
-            body: result.buffer.toString("base64"),
-            isBase64Encoded: true
-        };
-    } else {
-        return {
-            statusCode: result.statusCode,
-            body: JSON.stringify({message: result.message})
-        };
-    }
+    // if (!allowedRequest(event.queryStringParameters)) {
+    //     return {
+    //         statusCode: 403,
+    //         body: JSON.stringify({message: 'Go away please'})
+    //     };
+    // }
+    // const result = await doCaptureWork(event.queryStringParameters);
+    // if (result.statusCode === 200) {
+    //     return {
+    //         statusCode: 200,
+    //         headers: {
+    //             "Content-Type": `image/${result.responseType}`,
+    //         },
+    //         body: result.buffer.toString("base64"),
+    //         isBase64Encoded: true
+    //     };
+    // } else {
+    //     return {
+    //         statusCode: result.statusCode,
+    //         body: JSON.stringify({message: result.message})
+    //     };
+    // }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: 'Hello world, this is a test'})
+    };
 }
